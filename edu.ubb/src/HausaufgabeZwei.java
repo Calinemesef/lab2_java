08861bf2bd5bba4b185bb0edc70344177a2d9d0b
bogdanmozacu <61746661+bogdanmozacu@users.noreply.github.com>
2020-11-07 15:16:59 +0200

    Initial commit

---
+++ edu.ubb/src/HausaufgabeZwei.java
@@ -0,0 +1,11 @@
+/**
+ *     TODO Install JUnit org.junit.jupiter:junit-jupiter:5.4.2
+ *     Link https://www.jetbrains.com/help/idea/testing.html
+ *     Alt Enter auf Klass Name
+ */
+
+public class HausaufgabeZwei {
+    public static void main(String[] args) {
+
+    }
+}
